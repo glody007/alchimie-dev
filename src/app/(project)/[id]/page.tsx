7f779da07a8e42105c648dc0e2783373c9c7fea9
glody007 <glodymbutwile@gmail.com>
2024-02-25 08:54:26 +0200

    feat: code related schemas

---
+++ src/app/(project)/[id]/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ProjectPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { unstable_noStore as noStore } from "next/cache";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
 
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
@@ -23,8 +24,14 @@ export default async function Home() {
         </Link>
       </div>
       <div className="w-full flex-1 flex justify-center items-center">
-        <div className="relative w-[400px] h-[400px] rounded bg-slate-300">
-          <Image src={challenge.image} alt={challenge.description} fill />
+        <div className="flex flex-col gap-4">
+          <div className="relative w-[400px] h-[400px] rounded bg-slate-300 border">
+            <Image src={challenge.image} alt={challenge.description} fill />
+          </div>
+          <div className="flex flex-col justify-start gap-2">
+            <p className="first-letter:uppercase text-sm font-light">{challenge.description}</p>
+            <Button size="lg" className="mr-auto">Start</Button>
+          </div>
         </div>
       </div>
     </main>
